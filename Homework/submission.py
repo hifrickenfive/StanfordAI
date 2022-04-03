307e0a58985022901e8ae1410de6b263edbf732d
hifrickenfive <jasonalexanderchan@gmail.com>
2022-04-03 21:40:36 +1000

    Working code
    5 hrs :(

--- Homework/submission.py
+++ Homework/submission.py
@@ -73,7 +73,38 @@ def mutate_sentences(sentence: str) -> List[str]:
                 (Reordered versions of this list are allowed.)
     """
     # BEGIN_YOUR_CODE (our solution is 17 lines of code, but don't worry if you deviate from this)
-    raise Exception("Not implemented yet")
+    word_list = sentence.split()
+    MAX_LENGTH = len(word_list)
+
+    word_dict = collections.defaultdict(list) # create lookup
+    for i in range(len(word_list)-1):
+        word_dict[word_list[i]].append(word_list[i+1])
+
+    def dfs(sentences:list, current_sentence:list, lookup:dict, key:str, current_length:int, MAX_LENGTH:int):
+        current_sentence.append(key)
+        current_length += 1
+
+        if len(current_sentence) == MAX_LENGTH: # Base case
+            sentences.append(' '.join(current_sentence))
+            return
+        elif key not in lookup.keys(): # Block use of the last word of input sentence in the middle of new sentence
+            return
+        else:
+            for neighbour in lookup[key]:
+                if len(lookup[key]) > 1: # Spawn copy of list for each branch
+                    dfs(sentences,  current_sentence.copy(), lookup, neighbour, current_length, MAX_LENGTH)
+                else:
+                    dfs(sentences, current_sentence, lookup, neighbour, current_length, MAX_LENGTH)
+
+    all_sentences = list()
+    for key in word_dict.keys():
+        subset_of_all_sentences = list()
+        current_sentence = list()
+        dfs(subset_of_all_sentences, current_sentence, word_dict, key, 0, MAX_LENGTH)
+        all_sentences.append(subset_of_all_sentences)
+    all_sentences = sum(all_sentences, [])  # Flatten nested list
+
+    return all_sentences
     # END_YOUR_CODE
 
 
--- Homework/test.py
+++ Homework/test.py
@@ -1,7 +1,40 @@
-x = 1
+import collections
 
-y = 2
+def dfs(all_sentences:list, current_sentence:list, graph:dict, key:str, current_length:int, MAX_LENGTH:int):
+    current_sentence.append(key)
+    current_length += 1
 
-z = x + y
+    # Base case: new_sentence length equals original sentence
+    if len(current_sentence) == MAX_LENGTH:
+        all_sentences.append(' '.join(current_sentence))
+        return
+    else:
+        for neighbour in graph[key]:
+            if len(graph[key]) > 1: # Spawn another list for each branch
+                dfs(all_sentences,  current_sentence.copy(), graph, neighbour, current_length, MAX_LENGTH)
+            else:
+                dfs(all_sentences, current_sentence, graph, neighbour, current_length, MAX_LENGTH)
 
-print(y)
+
+def mutate_sentences(sentence):
+    word_list = sentence.split()
+    word_dict = collections.defaultdict(list)
+    for i in range(len(word_list)-1):
+        word_dict[word_list[i]].append(word_list[i+1])
+
+    MAX_LENGTH = len(word_list)
+
+    all_sentences = list()
+    for key in word_dict.keys():
+        subset_of_all_sentences = list()
+        current_sentence = list()
+        dfs(subset_of_all_sentences, current_sentence, word_dict, key, 0, MAX_LENGTH)
+        all_sentences.append(subset_of_all_sentences)
+    
+    return all_sentences
+
+
+if __name__ == '__main__':
+    sentence  = 'the cat and the mouse'
+    result = mutate_sentences(sentence)
+    print(result)
