189c4ea2e0cec5cf683ead434a753c10aaf8b478
hifrickenfive <jasonalexanderchan@gmail.com>
2023-02-04 22:44:15 +1100

    working 1e

--- CS244N/a4/nmt_model.py
+++ CS244N/a4/nmt_model.py
@@ -277,8 +277,16 @@ class NMT(nn.Module):
         ###     Tensor Stacking:
         ###         https://pytorch.org/docs/stable/torch.html#torch.stack
 
+        enc_hiddens_proj = self.att_projection(enc_hiddens)
+        Y = self.model_embeddings.target.weight[target_padded]
 
-
+        target_length, batch_size, embedding_size = Y.shape
+        for t in range(target_length):
+            Ybar_t = torch.cat((Y[t], o_prev), dim=1)
+            dec_state, o_t, e_t = self.step(Ybar_t, dec_state, enc_hiddens, enc_hiddens_proj, enc_masks)
+            combined_outputs.append(o_t)
+            o_prev = o_t
+        combined_outputs = torch.stack(combined_outputs, dim=0)
         ### END YOUR CODE
 
         return combined_outputs
